5bba370ad05f6fe6e1273a98a9f067779e30d2ad
Quinton Fults <coding.banjo@gmail.com>
2020-03-20 10:36:36 -0700

    usestate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class App extends Component {
+const App = () => {
 
-  render() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    )
+  const [ countState, setCountState ] = useState({
+    text: 'hotdog',
+    count: 0
+  })
+
+  const handleBtnClick = () => {
+    setCountState({ ...countState, count: countState.count + 1 })
   }
+
+  return (
+    <div>
+      <h1>{countState.text}</h1>
+      <h1>Count: {countState.count}</h1>
+      <button onClick={handleBtnClick}>Click Me</button>
+    </div>
+  )
 }
 
 export default App
